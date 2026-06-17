1763c6a16f401be182ba18f92b2923dd7060d4c1
pjs <pjwater727@gmail.com>
2026-06-17 10:48:37 +0900

    v2606171048

---
+++ src/DHBIMWATER.Revit/Commands/Quantity/QuantityRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHBIMWATER.Revit.Commands.Quantity
+{
+    public enum RequestId
+    {
+        None = 0,
+
+    }
+    public class QuantityRequest
+    {
+    }
+}
