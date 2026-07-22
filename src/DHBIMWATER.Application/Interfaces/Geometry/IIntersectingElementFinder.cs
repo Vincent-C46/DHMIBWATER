6babe9b117a707ac05d7d084a9ec052b9701a300
pjs <pjwater727@gmail.com>
2026-07-22 17:02:56 +0900

    v2607221702_커밋

--- src/DHBIMWATER.Application/Interfaces/Geometry/IIntersectingElementFinder.cs
+++
@@ -1,9 +0,0 @@
-using DHBIMWATER.Core.Quantity;
-
-namespace DHBIMWATER.Application.Interfaces.Geometry
-{
-    public interface IIntersectingElementFinder
-    {
-        IReadOnlyList<FaceDeduction> FindContactAreas(long referenceElementId);
-    }
-}
--- src/DHBIMWATER.Application/Interfaces/Geometry/INetFaceVisualizerRepo.cs
+++
@@ -1,8 +0,0 @@
-namespace DHBIMWATER.Application.Interfaces.Geometry
-{
-    /// <summary>선택 요소의 공제 후 순수 노출면을 Revit에 시각화한다.</summary>
-    public interface INetFaceVisualizerRepo
-    {
-        int Visualize(IReadOnlyList<long> elementIds);
-    }
-}
